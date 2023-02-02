a721fe1681a69e3eb5728d528c83c77e840f30c6
Florence-nyokabi <florencenyokabiwangui@gmail.com>
2023-02-02 21:09:33 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,46 @@
+import java.security.KeyStore.TrustedCertificateEntry
+
+fun main(args: Array<String>) {
+    // Variables
+    var group = "Adalab"
+    println(group)
+
+    var person = "Mary"
+    person = "Jackson"
+    person = "Linda"
+    println(person)
+
+    var adalabNo = 33
+    adalabNo = 45
+    println(adalabNo)
+
+    var codeHivePopulation = 100
+    codeHivePopulation = 95
+    println(codeHivePopulation)
+
+    val codeHivePopulation1 = 150
+    println(codeHivePopulation1)
+
+    // Datatypes
+    var letters: Byte = 26
+    letters = 127
+    var country: Short = 195
+    var ugangaPopulation: Int = 45850000
+    var worldPopulation: Long = 7888000000L
+    var gravity: Float = 9.8F
+    var pi: Double = 3.14157864565467
+    var initial: Char = 'a'
+    var fullName: String = "Florence"
+    var dayTime: Boolean = false
+
+
+    // Arrays
+    var myFriends = arrayOf("Mark", "Jacob", "Joan", "Steve")
+    var myFriendsAges = arrayOf(21, 22, 23, 24)
+    var rwanda = arrayOf(13460000, "Paul Kagame", "RWF", 1081.40, true)
+
+    
+
+
+
+}
